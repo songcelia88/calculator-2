80ae96207681b97cda2993d562bf82f8cbbd08ee
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-03 17:51:36 -0700

    taking in a list as an argument

--- calculator.py
+++ calculator.py
@@ -4,8 +4,8 @@ Using the arithmetic.py file from Calculator Part 1, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import *
-
+#from arithmetic import *
+from arithmeticList import *
 
 # Your code goes here
 
@@ -15,33 +15,45 @@ while True:
 	if tokens[0] == "q":
 		break
 	else:
-		num1 = float(tokens[1])
-		if len(tokens) > 2:
-			num2 = float(tokens[2])
+		num_list = tokens[1:]
+		try:
+			for i in range(len(num_list)):
+				num_list[i] = float(num_list[i])
+			# num1 = float(tokens[1])
+			# if len(tokens) > 2:
+			# 	num2 = float(tokens[2])
+			# pass
+		except ValueError:
+			print("Those are not numbers!")
+			continue
+		
 
 		if tokens[0] == "+":
-			answer = float(add(num1, num2))
+			answer = float(add(num_list))
 			print(answer)
 		elif tokens[0] == "-":
-			answer = float(subtract(num1, num2))
+			answer = float(subtract(num_list))
 			print(answer)
 		elif tokens[0] == "*":
-			answer = float(multiply(num1, num2))
+			answer = float(multiply(num_list))
 			print(answer)
 		elif tokens[0] == "/":
-			answer = float(divide(num1, num2))
+			answer = float(divide(num_list))
 			print(answer)
 		elif tokens[0] == "square":
-			answer = float(square(num1))
+			answer = square(num_list)
 			print(answer)
 		elif tokens[0] == "cube":
-			answer = float(cube(num1))
+			answer = cube(num_list)
 			print(answer)
 		elif tokens[0] == "pow":
-			answer = float(power(num1, num2))
+			if len(num_list)<2:
+				print("Does not compute! Not enough numbers")
+				continue
+			answer = power(num_list)
 			print(answer)
 		elif tokens[0] == "mod":
-			answer = float(mod(num1, num2))
+			answer = mod(num_list)
 			print(answer)
 		else:
 			print("Invalid Function")
